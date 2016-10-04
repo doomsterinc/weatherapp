6248c2940d85d7ea93bd88928181f0979b112400
doomsterinc <doomsterinc1@gmail.com>
2016-10-04 20:47:38 -0300

    add examples cllback 5

--- example-callback.js
+++ example-callback.js
@@ -6,5 +6,9 @@ request({
   url: url,
   json: true
 }, function(error, response, body){
-  
+  if (error) {
+    console.log('Unable to fetch weather');
+  } else {
+    console.log(JSON.stringify(body, null, 4));
+  }
 });
