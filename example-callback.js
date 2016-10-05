a3407782de0849f1aa8255389aaf371738b2017d
doomsterinc <doomsterinc1@gmail.com>
2016-10-05 11:31:03 -0300

    add modules 2

--- example-callback.js
+++ example-callback.js
@@ -1,6 +1,6 @@
 var request = require('request');
 var url = 'http://api.openweathermap.org/data/2.5/weather?appid=9e68a0b2ebf54fac8c4ac127281e612f&q=PortoAlegre&units=metric';
-
+var weather = require('./weather');
 
 request({
   url: url,
