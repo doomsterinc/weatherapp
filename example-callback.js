f8ebcf3f26e824810f950f6787eac8426ca9affa
doomsterinc <doomsterinc1@gmail.com>
2016-10-05 11:50:53 -0300

    add modules 3

--- example-callback.js
+++ example-callback.js
@@ -2,15 +2,6 @@ var request = require('request');
 var url = 'http://api.openweathermap.org/data/2.5/weather?appid=9e68a0b2ebf54fac8c4ac127281e612f&q=PortoAlegre&units=metric';
 var weather = require('./weather');
 
-request({
-  url: url,
-  json: true
-}, function(error, response, body){
-  if (error) {
-    console.log('Unable to fetch weather');
-  } else {
-    // console.log(JSON.stringify(body, null, 4));
-    var data = body;
-    console.log("It's " + data.main.temp + " in " + data.name + "!");
-  }
+weather(function(currentWeather){
+  console.log(currentWeather);
 });
--- weather.js
+++ weather.js
@@ -1,3 +1,18 @@
-module.exports = function () {
-  console.log('Got weather');
+var request = require('request');
+
+module.exports = function (callback) {
+  var currentWeather;
+  request({
+    url: url,
+    json: true
+  }, function(error, response, body){
+    if (error) {
+      console.log('Unable to fetch weather');
+    } else {
+      // console.log(JSON.stringify(body, null, 4));
+      var data = body;
+       currentWeather = "It's " + data.main.temp + " in " + data.name + "!";
+    }
+  });
+  callback(currentWeather);
 };
