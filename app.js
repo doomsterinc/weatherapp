6b5a83f45470689e2e884d4f962b6788f298b70a
doomsterinc <doomsterinc1@gmail.com>
2016-10-06 03:28:58 -0300

    Clean code

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var weather = require('./weather');
 var location = require('./location');
 
-//setup yargs o have --location or -l argument
 var argv = require('yargs')
 	.options('location', {
     alias: 'l',
@@ -12,31 +11,13 @@ var argv = require('yargs')
   .help('help')
   .argv;
 
-// weather(function(currentWeather){
-//   console.log(currentWeather);
-// });
-//
-// location(function(location) {
-//   if (!location) {
-//     console.log("Unable to guess location!");
-//     return;
-//   }
-//   console.log("city: " + location.city);
-//   console.log("lag/log: " + location.loc);
-// });
-
-//if location provided
-//  call weather(location, callback)
-//else
-//  call location
-//      call weather(location, callback)
 if (typeof argv.l === 'string' && argv.l.length > 0 ) {
-  console.log('Has location!');
+  console.log('Location was provided!');
   weather(argv.l, function(currentWeather){
     console.log(currentWeather);
   })
 } else {
-  console.log('No location given!');
+  console.log('Location was not provided!');
   location(function(location){
     if (location) {
       weather(location.city, function(currentWeather){
