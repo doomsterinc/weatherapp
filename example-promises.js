a13ee1611565268b0efa60e9b6ab2ce7583885b7
doomsterinc <doomsterinc1@gmail.com>
2016-10-07 02:57:49 -0300

    test promises 13

--- example-promises.js
+++ example-promises.js
@@ -18,30 +18,31 @@
 // }, function(error){
 //   console.log(error);
 // });
+var request = require('request');
 
 function getWeather(location){
-  //return Promise
-    //
-    var encodLocation = encodeURIComponent(location);
-    var url = 'http://api.openweathermap.org/data/2.5/weather?appid=9e68a0b2ebf54fac8c4ac127281e612f&q=' + encodLocation + '&units=metric';
+    return new Promise(function (resolve, response){
+      var encodLocation = encodeURIComponent(location);
+      var url = 'http://api.openweathermap.org/data/2.5/weather?appid=9e68a0b2ebf54fac8c4ac127281e612f&q=' + encodLocation + '&units=metric';
 
-    if (!location) {
-      return reject("No location provided");
-    }
-
-    request({
-      url: url,
-      json: true
-    }, function(error, response, body){
-      var errmsg, currentWeather;
-      if (error) {
-        errmsg = 'Unable to fetch weather';
-        reject(errmsg);
-      } else {
-        var data = body;
-        currentWeather = "It's " + data.main.temp + " in " + data.name + "!";
-        resolve(currentWeather);
+      if (!location) {
+        return reject("No location provided");
       }
+
+      request({
+        url: url,
+        json: true
+      }, function(error, response, body){
+        var errmsg, currentWeather;
+        if (error) {
+          errmsg = 'Unable to fetch weather';
+          reject(errmsg);
+        } else {
+          var data = body;
+          currentWeather = "It's " + data.main.temp + " in " + data.name + "!";
+          resolve(currentWeather);
+        }
+      });
     });
 };
 
