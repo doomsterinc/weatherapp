e8de43105ea76a8874ef59fc3486aa068c0beb2b
doomsterinc <doomsterinc1@gmail.com>
2016-10-07 02:54:57 -0300

    test promises 12

--- example-promises.js
+++ example-promises.js
@@ -22,7 +22,28 @@
 function getWeather(location){
   //return Promise
     //
-}
+    var encodLocation = encodeURIComponent(location);
+    var url = 'http://api.openweathermap.org/data/2.5/weather?appid=9e68a0b2ebf54fac8c4ac127281e612f&q=' + encodLocation + '&units=metric';
+
+    if (!location) {
+      return reject("No location provided");
+    }
+
+    request({
+      url: url,
+      json: true
+    }, function(error, response, body){
+      var errmsg, currentWeather;
+      if (error) {
+        errmsg = 'Unable to fetch weather';
+        reject(errmsg);
+      } else {
+        var data = body;
+        currentWeather = "It's " + data.main.temp + " in " + data.name + "!";
+        resolve(currentWeather);
+      }
+    });
+};
 
 getWeather('new york').then(function(currentweather){
   console.log(currentweather);
