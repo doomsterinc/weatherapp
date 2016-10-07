e4c98ee78118fbce2933ca27ce582a1be3b399ec
doomsterinc <doomsterinc1@gmail.com>
2016-10-07 02:50:34 -0300

    test promises 10

--- example-promises.js
+++ example-promises.js
@@ -1,20 +1,20 @@
-function doWork(data, callback) {
-  callback(done);
-}
-
-function doWorkPromise(data){
-  return new Promise(function(resolve, reject){
-    setTimeout(function(){
-      reject('everything is broken');
-    },1000);
-    // reject({
-    //   error: "something bad happened"
-    // });
-  });
-}
-
-doWorkPromise('some Data').then(function(data){
-  console.log(data);
-}, function(error){
-  console.log(error);
-});
+// function doWork(data, callback) {
+//   callback(done);
+// }
+//
+// function doWorkPromise(data){
+//   return new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//       reject('everything is broken');
+//     },1000);
+//     // reject({
+//     //   error: "something bad happened"
+//     // });
+//   });
+// }
+//
+// doWorkPromise('some Data').then(function(data){
+//   console.log(data);
+// }, function(error){
+//   console.log(error);
+// });
