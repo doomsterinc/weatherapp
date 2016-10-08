8f4259611be349074929f067175ef6ec7df22eb9
doomsterinc <doomsterinc1@gmail.com>
2016-10-08 02:30:51 -0300

    transform in promises 8

--- app.js
+++ app.js
@@ -13,9 +13,7 @@ var argv = require('yargs')
 
 if (typeof argv.l === 'string' && argv.l.length > 0 ) {
   console.log('Location was provided!');
-  // weather(argv.l, function(currentWeather){
-  //   console.log(currentWeather);
-  // })
+
 	weather(argv.l).then(function(currentWeather){
 		console.log(currentWeather);
 	}).catch(function(error){
@@ -23,15 +21,7 @@ if (typeof argv.l === 'string' && argv.l.length > 0 ) {
 	});
 } else {
   console.log('Location was not provided!');
-  // location(function(location){
-  //   if (location) {
-  //     weather(location.city, function(currentWeather){
-  //       console.log(currentWeather);
-  //     });
-  //   } else {
-  //     console.log("Unable to guess location!");
-  //   }
-  // });
+
 	location().then(function(loc){
 		return weather(loc.city);
 	}).then(function(currentWeather){
