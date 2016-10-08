c498ac1016101262238dc54467fffb606471f09d
doomsterinc <doomsterinc1@gmail.com>
2016-10-08 02:29:01 -0300

    transform in promises 7

--- location.js
+++ location.js
@@ -8,7 +8,7 @@ module.exports = function () {
       json: true
     }, function(error, response, body){
       if (error) {
-        reject();
+        reject("Unable to guess location!");
       } else {
         resolve(body);
       }
