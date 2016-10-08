2f95622b75b8136471430399cacbc2b558e2ab50
doomsterinc <doomsterinc1@gmail.com>
2016-10-08 02:13:16 -0300

    transform in promises 2

--- weather.js
+++ weather.js
@@ -1,6 +1,27 @@
 var request = require('request');
 
 
-  var encodLocation = encodeURIComponent(location);
-  
+module.exports = function (location) {
+  return new Promise(function(resolve, reject){
+    var encodLocation = encodeURIComponent(location);
+    var url = 'http://api.openweathermap.org/data/2.5/weather?appid=9e68a0b2ebf54fac8c4ac127281e612f&q=' + encodLocation + '&units=metric';
+    if (!location) {
+      return reject("No location provided");
+    }
+
+    request({
+      url: url,
+      json: true
+    }, function(error, response, body){
+      var errmsg, currentWeather;
+      if (error) {
+        errmsg = 'Unable to fetch weather';
+        reject(errmsg);
+      } else {
+        var data = body;
+        currentWeather = "It's " + data.main.temp + " in " + data.name + "!";
+        resolve(currentWeather);
+      }
+    });
+  });
 };
